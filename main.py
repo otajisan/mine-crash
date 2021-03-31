797522379990d24db5e280ec9f9af3df8a2544b4
Taji <taji.taji.otajisan@gmail.com>
2021-03-31 12:38:38 +0900

    fix: make scratch image

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 def make_mines():
-    global mines
+    global mines, mine_count
     mines = []
     mine_count = calc_mine_count()
     for index in range(mine_count):
         m = make_random_mine()
+        # avoid duplicate mine
         for n in mines:
-            # avoid duplicate mine
             if n[0] == m[0] and n[1] == m[1]:
                 continue
+        #print(m)
         flash_mine(m)
         mines.append(m)
-        #print(m)
-
-def calc_mine_count():
-    if stage <= 3:
-        return 3
-    elif stage <= 5:
-        return stage
-    return 5
-
-def flash_mine(mine):
-    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
-    for i in range(3):
-        led.plot(mine[0], mine[1])
-        basic.pause(100)
-    basic.pause(500)
-
 def make_random_mine():
+    global mine_x, mine_y
     mine_x = 0
     mine_y = 0
     # if mine position is right
-    while (mine_x == 0 and mine_y == 0) or (mine_x == 4 and mine_y == 4):
+    while mine_x == 0 and mine_y == 0 or mine_x == 4 and mine_y == 4:
         mine_x = randint(0, 4)
         mine_y = randint(0, 4)
     return [mine_x, mine_y]
-
+def goal():
+    global is_ready, stage
+    is_ready = False
+    music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
+    music.play_tone(147, music.beat(BeatFraction.EIGHTH))
+    stage += 1
+    basic.pause(2000)
+    reset()
 def judge_mine():
     for mine in mines:
         if eddie.get(LedSpriteProperty.X) == mine[0] and eddie.get(LedSpriteProperty.Y) == mine[1]:
             damage()
+def initialize_eddie():
+    eddie.set_direction(90)
+    eddie.set(LedSpriteProperty.X, 0)
+    eddie.set(LedSpriteProperty.Y, 0)
 
 def on_button_pressed_a():
-    global is_ready
     if is_ready:
         music.play_tone(330, music.beat(BeatFraction.EIGHTH))
         eddie.turn(Direction.RIGHT, 90)
         show_direction()
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    global is_ready
-    if is_ready:
-        music.play_tone(262, music.beat(BeatFraction.EIGHTH))
-        eddie.move(1)
-        judge_mine()
-        if eddie.get(LedSpriteProperty.X) == 4 and eddie.get(LedSpriteProperty.Y) == 4:
-            goal()
-input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def show_bomb():
     basic.show_animation("""
@@ -66,16 +52,19 @@ def show_bomb():
         # # # # #
         . # # # .
     """)
-
-def goal():
-    global is_ready, stage
-    is_ready = False
-    music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
-    music.play_tone(147, music.beat(BeatFraction.EIGHTH))
-    stage += 1
-    basic.pause(2000)
+def calc_mine_count():
+    if stage <= 3:
+        return 3
+    elif stage <= 5:
+        return stage
+    return 5
+def clear_all():
+    global mines, stage, life
+    mines = []
+    stage = 0
+    life = 3
     reset()
-
+    eddie.on()
 def show_direction():
     current_direction = eddie.direction()
     if current_direction == 90:
@@ -88,60 +77,63 @@ def show_direction():
         arrow = ArrowNames.NORTH
     basic.show_arrow(arrow)
 
+def on_button_pressed_b():
+    if is_ready:
+        music.play_tone(262, music.beat(BeatFraction.EIGHTH))
+        eddie.move(1)
+        judge_mine()
+        if eddie.get(LedSpriteProperty.X) == 4 and eddie.get(LedSpriteProperty.Y) == 4:
+            goal()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def show_current_life():
     basic.show_string("" + str(life))
-
+def reset():
+    initialize_eddie()
+    make_mines()
+    play_game_music()
 def damage():
     global is_ready, life
     if life > 0:
         is_ready = False
-        music.start_melody(music.built_in_melody(Melodies.POWER_DOWN), MelodyOptions.ONCE)
+        music.start_melody(music.built_in_melody(Melodies.POWER_DOWN),
+            MelodyOptions.ONCE)
         show_bomb()
         life += -1
         show_current_life()
         basic.pause(2000)
         initialize_eddie()
         play_game_music()
-
 def play_game_music():
     global is_ready
     if life > 0:
         music.start_melody(music.built_in_melody(Melodies.NYAN), MelodyOptions.ONCE)
         is_ready = True
-
-def reset():
-    initialize_eddie()
-    make_mines()
-    play_game_music()
-
-def initialize_eddie():
-    eddie.set_direction(90)
-    eddie.set(LedSpriteProperty.X, 0)
-    eddie.set(LedSpriteProperty.Y, 0)
-
-def clear_all():
-    global stage, life, mines
-    mines = []
-    stage = 0
-    life = 3
-    reset()
-    eddie.on()
-
-is_ready = False
 stage = 0
-life = 3
-eddie: game.LedSprite = game.create_sprite(0, 0)
+is_ready = False
+mine_y = 0
+mine_x = 0
+mine_count = 0
 mines: List[List[number]] = []
-
+life = 0
+eddie: game.LedSprite = None
+def flash_mine(mine: List[number]):
+    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
+    for i in range(3):
+        led.plot(mine[0], mine[1])
+        basic.pause(100)
+    basic.pause(500)
+life = 3
+eddie = game.create_sprite(0, 0)
 clear_all()
 
 def on_forever():
     if life == 0:
-        music.start_melody(music.built_in_melody(Melodies.WAWAWAWAA), MelodyOptions.ONCE)
+        music.start_melody(music.built_in_melody(Melodies.WAWAWAWAA),
+            MelodyOptions.ONCE)
         eddie.off()
         game.set_score(stage)
         basic.show_string("GAMEOVER")
         game.show_score()
         clear_all()
-
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,50 +2,42 @@ function make_mines() {
     let m: number[];
     
     mines = []
-    let mine_count = calc_mine_count()
+    mine_count = calc_mine_count()
     for (let index = 0; index < mine_count; index++) {
         m = make_random_mine()
+        //  avoid duplicate mine
         for (let n of mines) {
-            //  avoid duplicate mine
             if (n[0] == m[0] && n[1] == m[1]) {
                 continue
             }
             
         }
+        // print(m)
         flash_mine(m)
         mines.push(m)
     }
 }
 
-// print(m)
-function calc_mine_count(): number {
-    if (stage <= 3) {
-        return 3
-    } else if (stage <= 5) {
-        return stage
-    }
-    
-    return 5
-}
-
-function flash_mine(mine: number[]) {
-    music.playTone(523, music.beat(BeatFraction.Eighth))
-    for (let i = 0; i < 3; i++) {
-        led.plot(mine[0], mine[1])
-        basic.pause(100)
-    }
-    basic.pause(500)
-}
-
 function make_random_mine(): number[] {
-    let mine_x = 0
-    let mine_y = 0
+    
+    mine_x = 0
+    mine_y = 0
     //  if mine position is right
     while (mine_x == 0 && mine_y == 0 || mine_x == 4 && mine_y == 4) {
         mine_x = randint(0, 4)
         mine_y = randint(0, 4)
     }
     return [mine_x, mine_y]
+}
+
+function goal() {
+    
+    is_ready = false
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    music.playTone(147, music.beat(BeatFraction.Eighth))
+    stage += 1
+    basic.pause(2000)
+    reset()
 }
 
 function judge_mine() {
@@ -57,25 +49,17 @@ function judge_mine() {
     }
 }
 
+function initialize_eddie() {
+    eddie.setDirection(90)
+    eddie.set(LedSpriteProperty.X, 0)
+    eddie.set(LedSpriteProperty.Y, 0)
+}
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
     if (is_ready) {
         music.playTone(330, music.beat(BeatFraction.Eighth))
         eddie.turn(Direction.Right, 90)
         show_direction()
-    }
-    
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    if (is_ready) {
-        music.playTone(262, music.beat(BeatFraction.Eighth))
-        eddie.move(1)
-        judge_mine()
-        if (eddie.get(LedSpriteProperty.X) == 4 && eddie.get(LedSpriteProperty.Y) == 4) {
-            goal()
-        }
-        
     }
     
 })
@@ -89,14 +73,23 @@ function show_bomb() {
     `)
 }
 
-function goal() {
+function calc_mine_count(): number {
+    if (stage <= 3) {
+        return 3
+    } else if (stage <= 5) {
+        return stage
+    }
     
-    is_ready = false
-    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-    music.playTone(147, music.beat(BeatFraction.Eighth))
-    stage += 1
-    basic.pause(2000)
+    return 5
+}
+
+function clear_all() {
+    
+    mines = []
+    stage = 0
+    life = 3
     reset()
+    eddie.on()
 }
 
 function show_direction() {
@@ -115,8 +108,26 @@ function show_direction() {
     basic.showArrow(arrow)
 }
 
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    if (is_ready) {
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        eddie.move(1)
+        judge_mine()
+        if (eddie.get(LedSpriteProperty.X) == 4 && eddie.get(LedSpriteProperty.Y) == 4) {
+            goal()
+        }
+        
+    }
+    
+})
 function show_current_life() {
     basic.showString("" + ("" + life))
+}
+
+function reset() {
+    initialize_eddie()
+    make_mines()
+    play_game_music()
 }
 
 function damage() {
@@ -143,32 +154,25 @@ function play_game_music() {
     
 }
 
-function reset() {
-    initialize_eddie()
-    make_mines()
-    play_game_music()
-}
-
-function initialize_eddie() {
-    eddie.setDirection(90)
-    eddie.set(LedSpriteProperty.X, 0)
-    eddie.set(LedSpriteProperty.Y, 0)
-}
-
-function clear_all() {
-    
-    mines = []
-    stage = 0
-    life = 3
-    reset()
-    eddie.on()
-}
-
-let is_ready = false
 let stage = 0
-let life = 3
-let eddie = game.createSprite(0, 0)
+let is_ready = false
+let mine_y = 0
+let mine_x = 0
+let mine_count = 0
 let mines : number[][] = []
+let life = 0
+let eddie : game.LedSprite = null
+function flash_mine(mine: number[]) {
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    for (let i = 0; i < 3; i++) {
+        led.plot(mine[0], mine[1])
+        basic.pause(100)
+    }
+    basic.pause(500)
+}
+
+life = 3
+eddie = game.createSprite(0, 0)
 clear_all()
 basic.forever(function on_forever() {
     if (life == 0) {
